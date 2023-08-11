a4a8c5ea7b018afaac7fc432a13cde7abd53397a
mspilimbergo <marcospilimbergo@gmail.com>
2023-08-11 15:57:13 -0400

    dynamic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 import { Toaster } from "@/components/ui/toaster";
@@ -5,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+export const dynamic = 'force-dynamic'
 
 const inter = Inter({ subsets: ["latin"] });
 
