9c7e8ffef07b9d4c83eec33a61a524e97d50b444
mspilimbergo <marcospilimbergo@gmail.com>
2023-08-11 15:54:26 -0400

    Encoding

--- src/app/api/confirm-signup/route.js
+++ src/app/api/confirm-signup/route.js
@@ -1,7 +1,4 @@
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { createClient } from '@supabase/supabase-js';
-// import { cookies } from 'next/headers';
 import { NextResponse } from "next/server";
 
 const SupabaseAdmin = () => {
@@ -11,30 +8,26 @@ const SupabaseAdmin = () => {
     );
   };
 
-// export const dynamic = 'force-dynamic'
 
 export async function POST(req, res) {
   console.log("IN CONFIRM SIGNUP")
-    // const supabase = createServerComponentClient({cookies});
   if (req.method === 'POST') {
     try {
         const body = await req.json();
-        // const email = body['email']    
-        // const password = body['password']
-        // console.log("EMAIL", email)
-        // console.log("PASSWORD", password)
-        // const {data: {user}, error: adminError} = await SupabaseAdmin().auth.signUp({
-        //     email: email,
-        //     password: password
-        // })
+        const email = body['email']    
+        const password = body['password']
+        const {data: {user}, error: adminError} = await SupabaseAdmin().auth.signUp({
+            email: email,
+            password: password
+        })
 
-        // // const {data: newSignIn, error} = await supabase.auth.signInWithPassword({email, password});
-        // if (error) {
-        //   console.log("======ADMIN ERROR=====", adminError)
-        // }
-        return NextResponse.json({data: user})
-        // return NextResponse.redirect(`${process.env.CLIENT_URL}/dashboard`)
-    // res.send({url: session.url})
+        console.log('user', user)
+        
+        if (adminError) {
+          console.log("======ADMIN ERROR=====", adminError)
+          return NextResponse.json({data: "failure"})
+        }
+        return NextResponse.json({data: "success"})
     } catch (err) {
         return NextResponse.json(
             {
