05d9b0c7b44abbcef0204144ebee9e7e397e4376
mspilimbergo <marcospilimbergo@gmail.com>
2023-10-29 11:34:17 -0400

    Sign in navbar fix

--- src/components/SignInNavbar.tsx
+++ src/components/SignInNavbar.tsx
@@ -49,20 +49,26 @@ const SignInNavbar = () => {
 
 
   return (
-    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300  py-2 px-2">
+    <header>
+    <nav className="inset-x-0 -top:4 md:top-0 bg-white dark:bg-gray-950 z-[50] h-fit border-b border-zinc-300 p-1 md:px-20 ">
       <div className={"flex items-center justify-between h-full gap-2 md:px-0 mx-auto md:max-w-[1480px]"}> 
         {/* Logo */}
         <Link href={"/"} className="flex items-center gap-2">
-          <Image src={"/Logo.svg"} width={140} height={140} alt="loading" />
+            <Image src={"/Logo.svg"} width={140} height={140} alt="loading" />
+            {/* <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
+            Series 7 Practice
+          </p>           */}
+          
         </Link>
-        <div className="flex items-center">
+        <div className="flex items-center">          
             <div className="flex flex-row gap-4">
-              <CTAButton text={"Create an account"} />
-            </div>
-
+              {/* <SignInButton text={"Sign In"} /> */}
+              <CTAButton text={"Create An Account"} />
+            </div>        
         </div>
       </div>
-    </div>
+    </nav>
+    </header>
   );
 };
 
