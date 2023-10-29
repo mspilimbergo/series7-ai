9d213e6d238432474cf1ac4455b3668e617706dc
mspilimbergo <marcospilimbergo@gmail.com>
2023-10-29 11:39:26 -0400

    Fix dashboard navbar

--- src/components/dashboard/DashboardNavBar.tsx
+++ src/components/dashboard/DashboardNavBar.tsx
@@ -50,32 +50,26 @@ const DashboardNavBar = () => {
 
 
   return (
-    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300  py-2 ">
-      <div className={"flex items-center justify-between h-full gap-2 px-4 mx-auto md:max-w-7xl "}> 
+    <header>
+    <nav className="inset-x-0 -top:4 md:top-0 bg-white dark:bg-gray-950 z-[50] h-fit border-b border-zinc-300 p-1 md:px-20 ">
+      <div className={"flex items-center justify-between h-full gap-2 md:px-0 mx-auto md:max-w-[1480px]"}> 
         {/* Logo */}
-        <Link href={"/dashboard"} className="flex items-center gap-2">
-        <Image src={"/Logo.svg"} width={140} height={140} alt="loading" />
+        <Link href={"/"} className="flex items-center gap-2">
+            <Image src={"/Logo.svg"} width={140} height={140} alt="loading" />
             {/* <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
             Series 7 Practice
-          </p> */}
+          </p>           */}
           
         </Link>
-        <div className="flex items-center">
+        <div className="flex items-center">          
             <div className="flex flex-row gap-4">
               <ProfileButton text={"Edit Profile"} />
               <SignOutButton text={"Sign Out"} />
-            </div>
-            {/* <SignOutButton text={"Profile"} /> */}
-            {/* <div className="flex flex-row gap-4"> */}
-                
-              {/* <SignInButton text={"Sign In"} /> */}
-              {/* <CTAButton text={"Start Practicing"} /> */}
-              {/* <Button variant={'default'}>Start Practicing</Button> */}
-              {/* <SignInButton cta={false} text={"Start Practicing"} /> */}
-            {/* </div> */}
+            </div>        
         </div>
       </div>
-    </div>
+    </nav>
+    </header>
   );
 };
 
